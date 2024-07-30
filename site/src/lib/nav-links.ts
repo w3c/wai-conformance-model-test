630480ee4c6330bcb0b8239a93a1c7009b1af28c
Francis Storr <francis.storr@intel.com>
2024-07-30 11:42:47 -0700

    Re-order nav links so “about” is at the end

--- site/src/lib/nav-links.ts
+++ site/src/lib/nav-links.ts
@@ -1,10 +1,10 @@
 const links = [
-  { href: "/about", name: "About" },
   { href: "/collections", name: "Collections" },
   { href: "/tour", name: "Take a Tour" },
   { href: "/news", name: "In the News" },
   { href: "/gift-shop", name: "Gift Shop" },
   { href: "/map", name: "Museum Map" },
   { href: "/volunteer", name: "Volunteer" },
+  { href: "/about", name: "About" },
 ]
 export default links;

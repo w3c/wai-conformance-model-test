7f81a0910d5382a3a0b82ec1ade3f048feee5397
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-08-01 11:47:46 -0400

    Add workflow to deploy to GitHub pages (#6)
    * Update hrefs to support Astro base URL
    
    * Add GitHub workflow to deploy to gh-pages

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,6 +4,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://accessiblecommunity.github.io",
+  base: "fixable",
+  trailingSlash: "always",
   server: {
     host: true,
     port: 4323
--- site/src/lib/nav-links.ts
+++ site/src/lib/nav-links.ts
@@ -1,9 +1,12 @@
 export const navLinks = [
-  { href: "/collections", name: "Collections" },
-  { href: "/tour", name: "Take a Tour" },
-  { href: "/news", name: "In the News" },
-  { href: "/gift-shop", name: "Gift Shop" },
-  { href: "/map", name: "Museum Map" },
-  { href: "/volunteer", name: "Volunteer" },
-  { href: "/about", name: "About" },
-];
+  { href: "collections/", name: "Collections" },
+  { href: "tour/", name: "Take a Tour" },
+  { href: "news/", name: "In the News" },
+  { href: "gift-shop/", name: "Gift Shop" },
+  { href: "map/", name: "Museum Map" },
+  { href: "volunteer/", name: "Volunteer" },
+  { href: "about/", name: "About" },
+].map(({href, name}) => ({
+  href: `${import.meta.env.BASE_URL}${href}`,
+  name
+}));

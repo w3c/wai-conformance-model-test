f2866801633c1558faef668708e62c1b66f19de8
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-08-14 12:25:16 -0400

    Rework layout, incl. repurposed grid, and mobile breakpoints (#26)

--- site/src/lib/nav-links.ts
+++ site/src/lib/nav-links.ts
@@ -3,12 +3,12 @@ import { museumBaseUrl } from "./constants";
 export const navLinks = [
   { href: "collections/", name: "Collections" },
   { href: "tour/", name: "Take a Tour" },
-  { href: "news/", name: "In the News" },
-  { href: "gift-shop/", name: "Gift Shop" },
+  { href: "news/", name: "In the News", headerClass: "hidden-below-lg" },
+  { href: "gift-shop/", name: "Gift Shop", headerClass: "hidden-below-lg", footerClass: "hidden-below-lg" },
   { href: "map/", name: "Museum Map" },
   { href: "volunteer/", name: "Volunteer" },
   { href: "about/", name: "About" },
-].map(({href, name}) => ({
+].map(({href, ...rest}) => ({
   href: `${museumBaseUrl}${href}`,
-  name
+  ...rest
 }));
